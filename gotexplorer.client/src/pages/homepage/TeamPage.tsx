de891750ffe2c7849338a8ac836ea0f6a882bcdd
lizabre <lizabredichina@gmail.com>
2025-03-04 15:56:48 +0200

    added members photos

--- gotexplorer.client/src/pages/homepage/TeamPage.tsx
+++ gotexplorer.client/src/pages/homepage/TeamPage.tsx
@@ -1,109 +1,105 @@
 import TeamMember from "./TeamMember";
-import GOTlogo from "../../assets/images/GOTlogo.webp"; 
+import GOTlogo from "../../assets/images/GOTlogo.webp";
+import uliana from "../../assets/images/team_members/uliana.webp";
+import andrii from "../../assets/images/team_members/andrii.webp";
+import katya from "../../assets/images/team_members/katya.webp";
+import liza from "../../assets/images/team_members/liza.webp";
+import dima from "../../assets/images/team_members/dima.webp";
+import nastya from "../../assets/images/team_members/nastya.webp";
+import marie from "../../assets/images/team_members/marie.webp";
 
 
 const TeamPage = () => {
-  return (
-    <div className="team-page">
-      <div className="team-page__content">
-        <header className="team-page__header">
-          <img src={GOTlogo} alt="GOTExplorer Logo" />
-          <h2>Team</h2>
-        </header>
-        <div className="team-page__grid">
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-    
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          <TeamMember
-            name="Leo Arcand"
-            role="Software Engineer"
-            description="There are many variations of passages of Lorem Ipsum available"
-            image="https://via.placeholder.com/150"
-            socials={[
-              { icon: "facebook", link: "https://facebook.com" },
-              { icon: "instagram", link: "https://instagram.com" },
-              { icon: "twitter", link: "https://twitter.com" },
-            ]}
-          />
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="team-page">
+            <div className="team-page__content">
+                <header className="team-page__header">
+                    <img src={GOTlogo} alt="GOTExplorer Logo" />
+                    <h2>Team</h2>
+                </header>
+                <div className="team-page__grid">
+                    <TeamMember
+                        name="Uliana Yeshchenko"
+                        role="PM, Frontend developer"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={uliana}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                    <TeamMember
+                        name="Andrii Avdiiuk"
+                        role="Backend developer"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={andrii}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+
+                    <TeamMember
+                        name="Kateryna Pavlenko"
+                        role="Backend developer"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={katya}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                    <TeamMember
+                        name="Yelyzaveta Bredikhina"
+                        role="Frontend developer"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={liza}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                    <TeamMember
+                        name="Dmytro Levkovych"
+                        role="QA"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={dima}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                    <TeamMember
+                        name="Anastasia Grinke"
+                        role="3D artist, Fronted developer"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={nastya}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                    <TeamMember
+                        name="Maria Yakovenko"
+                        role="3D artist"
+                        description="There are many variations of passages of Lorem Ipsum available"
+                        image={marie}
+                        socials={[
+                            { icon: "facebook", link: "https://facebook.com" },
+                            { icon: "instagram", link: "https://instagram.com" },
+                            { icon: "twitter", link: "https://twitter.com" },
+                        ]}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default TeamPage;
