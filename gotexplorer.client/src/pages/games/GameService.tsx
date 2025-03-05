a003a06032671f0a3568c5fc3d745fc1ca034c0a
lizabre <lizabredichina@gmail.com>
2025-03-05 23:10:46 +0200

    bug fixes

--- gotexplorer.client/src/pages/games/GameService.tsx
+++ gotexplorer.client/src/pages/games/GameService.tsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
+import { getAuthConfig } from "../auth/GetAuthConfig";
 
-class GameService{
+
+class GameService {
     cookies = new Cookies(null, { path: '/' });
-    config = {
-        headers: { Authorization: `Bearer ${this.cookies.get('token')}` }
-    };
-     start_game() {
-        return  axios
-            .post("https://localhost:7079/api/game/start", null, this.config)
+    start_game() {
+        return axios
+            .post("https://localhost:7079/api/game/start", null, getAuthConfig())
             .then((response) => {
                 this.cookies.remove("gameid");
                 this.cookies.set("gameid", response.data.gameId);
@@ -16,30 +15,30 @@ class GameService{
                 return response.data;
             });
     }
-    load_level(id:number) {
+    load_level(id: number) {
         return axios.get(`https://localhost:5173/api/level/${id}`);
     }
-     calculate_level(levelId: number, x: number, y: number) {
-         const gameid = this.cookies.get("gameid");
-         console.log(gameid);
-         return axios.put(`https://localhost:7079/api/game/${gameid}/calculatescore`, {
+    calculate_level(levelId: number, x: number, y: number) {
+        const gameid = this.cookies.get("gameid");
+        console.log(gameid);
+        return axios.put(`https://localhost:7079/api/game/${gameid}/calculatescore`, {
             levelId,
             x,
             y
-        }, this.config);
+        }, getAuthConfig());
     }
-     complete_game() {
+    complete_game() {
         const gameid = this.cookies.get("gameid");
-        return  axios.put(`https://localhost:7079/api/game/${gameid}/complete`,
+        return axios.put(`https://localhost:7079/api/game/${gameid}/complete`,
             null,
-            this.config).then(() => {
+            getAuthConfig()).then(() => {
                 this.cookies.remove("gameid");
                 this.cookies.remove("levelIds");
             });
     }
     getLeaderboard() {
         console.log("Making GET request to leaderboard");
-        return axios.get("https://localhost:7079/api/leaderboard", this.config);
+        return axios.get("https://localhost:7079/api/leaderboard", getAuthConfig());
     }
 }
 export default new GameService();
--- gotexplorer.client/src/pages/games/StartGamePage.tsx
+++ gotexplorer.client/src/pages/games/StartGamePage.tsx
@@ -3,7 +3,7 @@ import Footer from "../additional_components/Footer";
 import Navigation from "../additional_components/Navigation";
 import GameService from "./GameService";
 import './StartGamePage.scss';
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const StartGamePage = () => {
     const gameserv = GameService;
@@ -29,15 +29,19 @@ const StartGamePage = () => {
         }
         navigate("/lvl/game");
     };
-    return (<div className="start-game-page">
-        <Navigation />
-        <div className="main-content">
-            <div className="text">LET'S EXPLORE THE WORLD</div>
-            <button className="button-start" onClick={handleGameClick}>START NOW</button>
-        </div>
+    return (
+        <>{isAuthenticated ?
+            <div className="start-game-page">
+                <Navigation />
+                <div className="main-content">
+                    <div className="text">LET'S EXPLORE THE WORLD</div>
+                    <button className="button-start" onClick={handleGameClick}>START NOW</button>
+                </div>
 
-        <Footer />
-    </div>);
+                <Footer />
+            </div > :
+            <Navigate to="/login"></Navigate>}
+        </>);
 }
 
 export default StartGamePage;
