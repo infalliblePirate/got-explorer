a4a133f92d4c097cd4f90e7b39e1cfa8aba9e8ac
Ulia <ulianaescha05@gmail.com>
2025-03-14 12:29:16 +0200

    api url change

--- gotexplorer.client/src/services/api.ts
+++ gotexplorer.client/src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "/api";
 
 const api = axios.create({
-  baseURL: API_URL + "/api",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
